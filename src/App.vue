42065d61cd41efef598f744c7971435d3d48a210
hzshaqihe <hzshaqihe@corp.netease.com>
2017-03-22 21:37:28 +0800

    @misc 引入sass文件

--- src/App.vue
+++ src/App.vue
@@ -16,13 +16,14 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+@import "./style/var.scss";
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: $light-blue;
   margin-top: 60px;
 }
 </style>
